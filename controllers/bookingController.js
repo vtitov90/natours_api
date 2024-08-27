7bc99d0efb986f95f3235c728e07aabaac8ea368
vtitov <iqronaft@gmail.com>
2024-08-27 09:10:53 +0300

    booking fixed

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -59,7 +59,7 @@ const createBookingCheckout = async (session) => {
   console.log(session);
   const tour = session.client_reference_id;
   const user = (await User.findOne({ email: session.customer_email }))._id; //.id ?
-  const price = session.line_items[0].price_data.unit_amount / 100;
+  const price = session.amount_total / 100;
   await Booking.create({ tour, user, price });
 };
 
