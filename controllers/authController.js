780af6865a77152b545e9c783bf712469dd40261
vtitov <iqronaft@gmail.com>
2025-05-08 22:24:14 +0300

    controllers tests added

--- controllers/authController.js
+++ controllers/authController.js
@@ -16,7 +16,7 @@ const signRefreshToken = (id) =>
     expiresIn: process.env.REFRESH_TOKEN_EXPIRES_IN,
   });
 
-const createSendToken = async (user, statusCode, req, res) => {
+exports.createSendToken = async (user, statusCode, req, res) => {
   const accessToken = signToken(user._id);
   const refreshToken = signRefreshToken(user._id);
 
